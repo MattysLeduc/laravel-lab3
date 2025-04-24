fdeb0779ae4eff87b682510ccbf7b6838b0f73ee
MattysLeduc <mattys.leduc@gmail.com>
2025-04-24 16:22:09 -0400

    added code to create data in the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Author::factory(5)
+        ->has(Post::factory(3)
+            ->has(Comment::factory(4), 'comments'),
+        'posts')
+        ->create();
     }
 }
