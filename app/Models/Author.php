979ff107bdbd1c757644082f3a27881cd23ee442
MattysLeduc <mattys.leduc@gmail.com>
2025-04-24 16:16:11 -0400

    Added code to models

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     //
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
